c0e8eb567e2688d5e04ee1f16bb429d18a67b08f
Kasturi Sambarekar <kasturi.sambarekar@gmail.com>
2026-09-21 21:39:24 +0530

    Update App.jsx

--- shopnest/src/App.jsx
+++ shopnest/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
@@ -12,27 +10,22 @@ import Wishlist from "./pages/Wishlist";
 import NotFound from "./pages/NotFound";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts, fetchCategories } from "./store/productSlice";
-
 function ScrollToTop() {
   const { pathname } = useLocation();
   React.useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [pathname]);
-
   return null;
 }
-
 function App() {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
-
   React.useEffect(() => {
     if (!products.length) {
       dispatch(fetchProducts());
       dispatch(fetchCategories());
     }
   }, [dispatch, products.length]);
-
   return (
     <div className="app-shell">
       <ScrollToTop />
@@ -51,5 +44,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
