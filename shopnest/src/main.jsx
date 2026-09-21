57cc2a6dc6a03ed014e4dc46393719956e366f21
Kasturi Sambarekar <kasturi.sambarekar@gmail.com>
2026-09-21 21:57:43 +0530

    Remove unnecessary blank line in main.jsx

--- shopnest/src/main.jsx
+++ shopnest/src/main.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./store/store";
 import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
